77f60095d7649bd3a06369c1b8b919fd44841ec4
adamkali <adamkali@outlook.com>
2023-02-06 10:09:52 -0500

    add better script and wallpaper

--- install.sh
+++ install.sh
@@ -29,18 +29,43 @@ function run_command() {
   current_step=$((current_step + 1))
 }
 
-run_command "sudo pacman -Syyuu --noconfirm && sudo pacman -S --needed git base-devel &>/dev/null && git clone https://aur.archlinux.org/yay.git &>/dev/null && cd yay && makepkg -si &>/dev/null" "Installing YAY"
-run_command "yay -S leftwm wezterm eww neofetch rofi ttf-joypixels picom-ibhagwan-git leftwm-theme-git" "Installing AUR Package ttf-joypixels"
-run_command "curl -LJO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/VictorMono.zip &> /dev/null && unzip VictorMono.zip -d ~/.local/share/fonts &> /dev/null && fc-cache -f" "Installing VictorMono"
-run_command "git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim" "Cloning Packer"
-run_command "cp ~/arch-bootstrap/config/ ~/.config/" "Creating .config directory"
-run_command "git clone --depth 1 https://github.com/adamkali/FishConfig.git &>/dev/null && cp ./FishConfig/config.fish ~/.config/fish" "Cloning FishConfig"
-run_command "leftwm-theme apply vapor" "Install theme"
-run_command "pip install pyright" "Installing pyright"
-run_command "go install golang.org/x/tools/gopls@latest" "Installing golang-lsp"
-run_command "cargo install texlab" "Installing Texlab"
-run_command "npm install -g typescript typescript-language-server svelte-language-server @tailwindcss/language-server" "Installing npm packages for language servers"
-run_command "rm -rf ./FishConfig &>/dev/null && rm -rf ./arch-bootstrap &>/dev/null" "cleaning up"
+sudo pacman -Syyuu --noconfirm 
+clear
+sudo pacman -S feh --noconfirm
+clear
+sudo pacman -S --needed git base-devel
+clear
+git clone https://aur.archlinux.org/yay.git 
+clear
+cd yay && makepkg -si && cd ../ && rm -rf yay
+clear
+yay -S leftwm wezterm eww neofetch rofi ttf-joypixels picom-ibhagwan-git leftwm-theme-git python-pip
+clear
+curl -LJO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/VictorMono.zip && unzip VictorMono.zip -d ~/.local/share/fonts  && fc-cache -f
+clear
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+clear
+cp ~/arch-bootstrap/config/ ~/.config/ 
+clear
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+clear
+echo "exec leftwm >> ~/.xinitrc"
+clear
+echo -ne "
+if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then \n
+  exec startx \n
+fi
+"
+clear
+pip install pyright
+clear
+go install golang.org/x/tools/gopls@latest
+clear
+cargo install texlab
+clear
+npm install -g typescript typescript-language-server svelte-language-server @tailwindcss/language-server
+clear
+rm -rf ~/arch-bootstrap &>/dev/null 
 
 echo -e "\n\033[0;32mAdam's Bootstrap completed successfully\033[0m"
 
